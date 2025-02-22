2c945fc44bc9dc3c15d720c1610656c3abcda0ed
robustov <yaroslavrobustov@yandex.ru>
2025-02-22 20:02:46 +0300

    первая нерабочая версия. Не скоадывает переменные, только числа

--- Formula.java
+++ Formula.java
@@ -1,3 +1,77 @@
 public class Formula {
-    
+    int pos;
+    String formula;
+    double res;
+
+    public Formula(String form) {
+        this.pos = 0;
+        this.formula = form;
+        this.res = 0;
+    }
+
+    public char curr() {
+        if (pos < formula.length()) {
+            System.out.println(formula.charAt(pos));
+            System.out.println(res);
+            return formula.charAt(pos);
+        }
+        else {
+            return '$';
+        }
+    }
+
+    public double prepare() {
+        res = this.add();
+        return res;
+
+    }
+
+    public double add() {
+        res = this.mult();
+        while (curr() == '+' || curr() == '-') {
+            double tmp = mult();
+            pos++;
+            if (curr() == '+') {
+                res = res + tmp;
+            }
+            else if (curr() == '-'){
+                res = res - tmp;
+            }
+        }
+        return res;
+    }
+
+    public double mult() {
+        res = this.group();
+        while (curr() == '*' || curr() == '/') {
+            double tmp = group();
+            pos++;
+            if (curr() == '*') {
+                res = res * tmp;
+            }
+            else {
+                res = res / tmp;
+            }
+        }
+        return res;
+    }
+
+    public double group() {
+        res = this.num();
+        return res;
+    }
+
+    public double num() {
+        StringBuilder tmp = new StringBuilder();
+        while (Character.isDigit(curr())) {
+            tmp.append(curr());
+            pos++;
+        }
+        if (tmp.toString().isEmpty()) {
+            return 0;
+        }
+        return Double.parseDouble(tmp.toString());
+    }
+
+
 }
--- Main.java
+++ Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        Formula f = new Formula("1+1+1+1");
+        f.prepare();
+        System.out.println(f.res);
 
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
-        }
+
     }
 }
